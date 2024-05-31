b32d8d8fe7ba1f5e6354bb95cd7800e2403a004f
Mrtn Anastasiya <martirosianam@mail.ru>
2024-05-31 08:58:28 +0300

    Добавила новый код в test_task3.py

--- test_task3.py
+++ test_task3.py
@@ -1,59 +1,52 @@
 # Юнит-тест для ДЗ "Секретарь".
 
 import unittest
-from task3 import get_name, get_directory, add
+from task3 import get_name, get_directory, add, documents, directories
+
 
 class Task3TestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.documents = documents
+        self.directories = directories
+
     def test_get_name(self):
-        documents = [
-            {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
-            {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
-            {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"},
-            {"type": "driver license", "number": "5455 028765", "name": "Василий Иванов"},
-        ]
 
-        directories = {
-            '1': ['2207 876234', '11-2', '5455 028765'],
-            '2': ['10006'],
-            '3': []
-        }
+        result_name_1 = get_name("10006")
+        self.assertEqual(result_name_1,
+                              'Аристарх Павлов',
+                              'Документ не найден')
 
-        result_name_1 = (get_name("10006"))
-        self.assertEqual(result_name_1 ,
-                            'Аристарх Павлов',
-                            'Документ не найден')
+        result_name_2 = get_name("101")
+        self.assertIsNotNone(result_name_2,
+                          'Документ не найден')
 
-        result_directory_1 = (get_directory("11-2"))
+        result_name_3 = get_name("311 020203")
+        self.assertIsNotNone(result_name_3,
+                          'Документ не найден')
+
+        result_name_4 = get_name("311 020203")
+        self.assertEqual(result_name_4,
+                         'Александр Пушкин',
+                         'Документ не найден')
+
+    def test_get_directory(self):
+
+        result_directory_1 = get_directory("11-2")
         self.assertEqual(result_directory_1,
                          '1',
                          'Полки с таким документом не найдено')
 
-        result_name_2 = (get_name("101"))
-        self.assertIn(result_name_2,
-                         documents,
-                         'Документ не найден')
-
-        new_document = add('international passport', '311 020203', 'Александр Пушкин', 3)
-        self.assertIn(new_document,
-                         documents,
-                         'Документ не найден')
-
-        result_directory_2 = (get_directory("311 020203"))
-        self.assertIn(result_directory_2,
-                         documents,
+        result_directory_2 = get_directory("311 020203")
+        self.assertIsNone(result_directory_2,
                          'Полки с таким документом не найдено')
 
-        result_name_3 = (get_name("311 020203"))
-        self.assertIn(result_name_3,
-                         documents,
-                         'Документ не найден')
+        result_directory_3 = get_directory("311 020204")
+        self.assertIsNone(result_directory_3,
+                          'Полки с таким документом не найдено')
 
-        result_directory_3 = (get_directory("311 020204"))
-        self.assertIn(result_directory_3,
-                      documents,
-                      'Полки с таким документом не найдено')
-
+    def test_add(self):
+        new_document = add('international passport', '311 020203', 'Александр Пушкин', 3)
 
 
 
